bb4b98f476a0c7ab9d97bfbe27108513e5e4189a
SHURENGEREL SERGELEN <140506003+sergelen02@users.noreply.github.com>
2025-10-13 15:34:10 +0000

    modified:   internal/ds/verify.go

--- internal/ds/verify.go
+++ internal/ds/verify.go
@@ -6,26 +6,25 @@ import (
 	"github.com/sergelen02/HPPK_2/internal/core"
 )
 
-// Verify compares (Q'(F) == P'(H)) in F_p for linear polynomials:
-//   left  = (q0*F) + (q1*F)*x   mod p
-//   right = (p0*H) + (p1*H)*x   mod p
 func Verify(pk *Public, msg []byte, sig *Signature) bool {
 	P := pk.P
 	Fp := core.NewField(P)
-
 	x := core.HashToX(P, msg)
-	F := new(big.Int).SetBytes(sig.F)
-	H := new(big.Int).SetBytes(sig.H)
 
-	// U(H) = p0*H + p1*H*x  (mod p)
-	U0 := Fp.Mul(pk.Pprime0, H)          // p0*H
-	U1 := Fp.Mul(pk.Pprime1, H)          // p1*H
-	right := Fp.Add(U0, Fp.Mul(U1, x))   // p0*H + (p1*H)*x
+	F := new(big.Int).SetBytes(sig.F) // f(x) mod p
+	H := new(big.Int).SetBytes(sig.H) // h(x) mod p
 
-	// V(F) = q0*F + q1*F*x  (mod p)
-	V0 := Fp.Mul(pk.Qprime0, F)          // q0*F
-	V1 := Fp.Mul(pk.Qprime1, F)          // q1*F
-	left := Fp.Add(V0, Fp.Mul(V1, x))    // q0*F + (q1*F)*x
+	// U(H) = (p0*H) + (p1*H)*x  over F_p
+	U0 := new(big.Int).Mod(new(big.Int).Mul(pk.Pprime0, H), P)
+	U1 := new(big.Int).Mod(new(big.Int).Mul(pk.Pprime1, H), P)
+
+	// V(F) = (q0*F) + (q1*F)*x  over F_p
+	V0 := new(big.Int).Mod(new(big.Int).Mul(pk.Qprime0, F), P)
+	V1 := new(big.Int).Mod(new(big.Int).Mul(pk.Qprime1, F), P)
+
+	left  := Fp.Add(V0, Fp.Mul(V1, x))
+	right := Fp.Add(U0, Fp.Mul(U1, x))
 
 	return left.Cmp(right) == 0
 }
+
