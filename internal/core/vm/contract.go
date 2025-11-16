fee7fdd1a6aefd579c4bd663dcda26a194c04097
SHURENGEREL SERGELEN <140506003+sergelen02@users.noreply.github.com>
2025-11-16 12:49:34 +0000

    deleted:    cmd/loadgen/main.go 	modified:   ct.json 	modified:   internal/core/vm/contract.go 	renamed:    internal/ds/golden_teest.go -> internal/ds/golden_test.go

--- internal/core/vm/contract.go
+++ internal/core/vm/contract.go
@@ -7,5 +7,3 @@ var PrecompiledContractsHomestead = map[common.Address]PrecompiledContract{
 	// ... 기존 항목들
 	common.HexToAddress("0x00000000000000000000000000000000000000F5"): HPPKVerifyPrecompile{},
 }
-
-// 필요한 다른 하드포크 맵(Berlin, London 등)에도 동일 등록
